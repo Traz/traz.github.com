ff51e235207760ff3e77b2b5f6fc86b95bbe4496
Traz <a.reith@gmail.com>
2013-04-20 22:20:32 +1100

    Transfert

--- _site/ntr.sh
+++
@@ -1,7 +0,0 @@
-jekyll build
-git add .
-git commit -m 'transfert'
-git devtree
-git co master
-git push -f origin master
-git co dev
--- _site/readme.md
+++
@@ -1,6 +0,0 @@
-Hi all !
-
-Just some off my remind / notes about Ruby and Rails. Fell free to fork them !
-
--- 
-Traz
